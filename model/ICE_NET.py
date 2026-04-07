afeeae1001e6bb9afed88e17f50d035d1b6f15bb
BARKKNIGHT <146512484+BARKKNIGHT@users.noreply.github.com>
2026-04-08 02:07:56 +0530

    added nn.AdaptiveAvgPool2d(1)

--- model/ICE_NET.py
+++ model/ICE_NET.py
@@ -14,6 +14,8 @@ class ICE_NET(nn.Module):
 
         self.relu = nn.ReLU(inplace = True)
 
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
         self.fc1 = nn.Linear(3136, 512)
         self.fc2 = nn.Linear(512, self.num_classes)
 
@@ -22,7 +24,8 @@ class ICE_NET(nn.Module):
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.relu(self.bn3(self.conv3(x)))
 
-        x = x.view(x.size(0), -1)
+        # x = x.view(x.size(0), -1)
+        x = self.avg_pool(x)
 
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
